7464148f9f23322b6005066cf01a7769e653c5c2
Shivamkumar8595 <102303566+Shivamkumar8595@users.noreply.github.com>
2022-11-08 21:13:37 +0530

    Add files via upload

---
+++ MyProjectLabDSA03/src/balancedbrackets/BalancedBracketCheckerTest.java
@@ -0,0 +1,36 @@
+package balancedbrackets;
+
+public class BalancedBracketCheckerTest {
+	
+	public static void main(String[] args) {
+		
+		test("");
+		test("([[{}]])");
+		
+		test("([[{}]]))");
+
+		test("[]");
+
+		test("[)");
+		
+		test("[[[]]]");
+
+		test("M{{[[[()]]]}}W");
+		test("M{{[[[()]]]}}");
+	}
+
+
+	static void test(String brackets) {
+		
+		BalancedBracketsChecker checker = new BalancedBracketsChecker(
+			brackets);
+		
+		boolean result = checker.check();
+		if (result) {
+			System.out.println("Bracket expression " + brackets + " is balanced");
+		}else {
+			System.out.println("Bracket expression " + brackets + " is NOT balanced");			
+		}		
+	}
+
+}
